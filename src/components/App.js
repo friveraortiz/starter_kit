0a586ed7d8c22c0a192485177eacc317e023bbc2
friveraortiz <61832284+friveraortiz@users.noreply.github.com>
2024-01-30 13:49:20 +0000

    Part 5
    I followed the fifth part of the tutorial called: Part 5: Sell Products (Front End)
    I added and modified the files that were necessary to make these two parts work.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json'
 import Navbar from './Navbar'
+import Main from './Main'
 
 class App extends Component {
 	
@@ -34,12 +35,20 @@ class App extends Component {
 	const networkData = Marketplace.networks[networkId]
 	if(networkData) {
        const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
+	   this.setState({ marketplace })
+	   const productCount = await marketplace.methods.productCount().call()
+	   console.log(productCount.toString())
+	   this.setState({ loading: false })
 	   console.log(marketplace)
 	} else{
       window.alert('Marketplace contract not deployed to detected network.')		
 	}
   }
   
+  // We will create a constructor related to the properties.
+  // A props constructor is a method for the React class component that takes a props parameter, 
+  // which stands for properties. 
+  // The props are data passed to this component from its parent.
   constructor(props) {
     super(props)
     this.state = {
@@ -48,38 +57,31 @@ class App extends Component {
       products: [],
       loading: true
     }
-  }  
+	
+	this.createProduct = this.createProduct.bind(this)
+  }
+  
+  createProduct(name, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
   
   render() {
     return (
       <div>
-	    <Navbar account={this.state.account}/>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
+           <div className="row">
+              <main role="main" className="col-lg-12 d-flex">
+                { this.state.loading
+                  ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                  : <Main createProduct={this.createProduct} />
+                }
+              </main>
+		   </div>
         </div>
       </div>
     );
